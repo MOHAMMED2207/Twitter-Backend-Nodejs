8fbb3920f0e73bf4497b8988e133157a836af439
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-05-18 03:46:32 +0300

    Update Auth.cjs

--- Server/Routes/Auth.cjs
+++ Server/Routes/Auth.cjs
@@ -3,16 +3,16 @@ const AuthControllr = require("../Controllers/AuthControllr.cjs");
 const AUTH_MIDDLEWARES = require("../middlewares/authentication.cjs"); // Routes
 const router = Express.Router(); // Routes
 
-router.get("api/auth/register", AuthControllr.GetAllUser); //
+router.get("/auth/register", AuthControllr.GetAllUser); //
 router.get(
-  "api/auth/register/:id",
+  "/auth/register/:id",
   AUTH_MIDDLEWARES,
   AuthControllr.GetUserProfile
 ); //
-router.post("api/auth/register", AuthControllr.register); //
+router.post("/auth/register", AuthControllr.register); //
 // ...........................................................................................................
-router.post("api/auth/login", AuthControllr.login); //
+router.post("/auth/login", AuthControllr.login); //
 // ...........................................................................................................
-router.post("api/auth/logout", AuthControllr.logout); //
+router.post("/auth/logout", AuthControllr.logout); //
 
 module.exports = router; //  Exports
