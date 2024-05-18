4df877fc695810f839511e73385e2ba454e7d11f
Mohamed Abdul Fatah <120588577+MOHAMMED2207@users.noreply.github.com>
2024-05-18 03:47:04 +0300

    Update Posts.cjs

--- Server/Routes/Posts.cjs
+++ Server/Routes/Posts.cjs
@@ -4,31 +4,31 @@ const AUTH_MIDDLEWARES = require("../middlewares/authentication.cjs"); // Routes
 
 const router = Express.Router();
 
-router.post("api/post/create", AUTH_MIDDLEWARES, POSTControllr.createPost);
+router.post("/post/create", AUTH_MIDDLEWARES, POSTControllr.createPost);
 router.delete(
-  "api/post/delete/:id",
+  "/post/delete/:id",
   AUTH_MIDDLEWARES,
   POSTControllr.deletePost
 );
 router.post(
-  "api/post/comment/:id",
+  "/post/comment/:id",
   AUTH_MIDDLEWARES,
   POSTControllr.commentOnPost
 );
 router.post(
-  "api/post/like/:id",
+  "/post/like/:id",
   AUTH_MIDDLEWARES,
   POSTControllr.likeUnlikePost
 );
-router.get("api/post/all", AUTH_MIDDLEWARES, POSTControllr.getAllPosts);
-router.get("api/post/likes/:id", AUTH_MIDDLEWARES, POSTControllr.getLikedPosts);
+router.get("/post/all", AUTH_MIDDLEWARES, POSTControllr.getAllPosts);
+router.get("/post/likes/:id", AUTH_MIDDLEWARES, POSTControllr.getLikedPosts);
 router.get(
-  "api/post/following",
+  "/post/following",
   AUTH_MIDDLEWARES,
   POSTControllr.getFollowingPosts
 );
 router.get(
-  "api/post/user/:username",
+  "/post/user/:username",
   AUTH_MIDDLEWARES,
   POSTControllr.getUserPosts
 );
